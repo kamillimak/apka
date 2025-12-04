ac677553aaf9866bf19acffe076d2d408993a655
Kamil <mikolajczykamil@gmail.com>
2025-12-04 01:10:25 +0100

    fix: Remove unused

--- src/components/sidebar/ColorCorrection.tsx
+++ src/components/sidebar/ColorCorrection.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { SelectionContext } from '../../context/SelectionContext';
 import { ColorCorrectionContext } from '../../context/ColorCorrectionContext';
 
